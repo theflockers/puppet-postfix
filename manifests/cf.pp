5f2f938257ac6085aeab9a90ba343ef16b073c27
Leandro Mendes <lmendes@brandwatch.com>
2018-08-13 12:18:04 +0200

    adding a new library to create tempdir

--- manifests/cf.pp
+++ manifests/cf.pp
@@ -3,16 +3,20 @@ class postfix::cf (
   Hash $main   = {}, 
   Array $master = []
 ) {
+
+  # create the temporary postfix directory
+  $tempdir = Postfix::Util::Tempdir.new("postfix-cf-dir")
+
   # merge default parameters with the given parameters
   $config = deep_merge($main, $postfix::params::defaults)
-  file { '/etc/postfix/main.cf':
+  file { "${tempdir}/main.cf":
     ensure  => file,
     content => template('postfix/main.cf.dynamic.erb'),
     notify  => Service['postfix']
   }
 
   $transports = union($master, $postfix::params::default_transports)
-  file { '/etc/postfix/master.cf':
+  file { '${tempdir}/master.cf':
     ensure  => file,
     content => template('postfix/master.cf.erb'),
     notify  => Service['postfix']
